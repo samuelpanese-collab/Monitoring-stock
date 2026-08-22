6e9ad2725dd22a02d8e8bd92217a091aef7a1729
Samueluy Panese <samuelpanese@gmail.com>
2026-08-22 11:46:20 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDim3ES_wbCVkhduvjLAbg7Z7Ze-lcWa00",
+  apiKey: "wThRxYu3bINtN9A4sacvMokUgpi2",
   authDomain: "monitoring-stock-b86d1.firebaseapp.com",
   projectId: "monitoring-stock-b86d1",
   storageBucket: "monitoring-stock-b86d1.firebasestorage.app",
